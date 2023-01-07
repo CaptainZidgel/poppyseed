fc164e5b7acdf1a1234c9570e28afd7574d045bb
Michael <thesentientgelatin@gmail.com>
2023-01-07 13:25:54 -0800

    more algorithms

--- pop.py
+++ pop.py
@@ -10,6 +10,9 @@ class Node():
         self.loc = loc
         self.t = time
 
+    def __sub__(self, other):
+        return abs(self.loc - other.loc)
+
     def __str__(self):
         return f"Node:l({self.loc}), t({self.t})"
 
@@ -32,7 +35,7 @@ class Space(list):
     def add(self, loc, t):
         self.append(Node(loc, t))
 
-    def match(self, p, q):
+    def match(self, p, q, t):
         self.remove(p)
         self.remove(q)
         match = (p, q, t)
@@ -52,6 +55,42 @@ class Space(list):
             diffs.append(m[2] - later.t)
         return diffs
 
+    def get_all_costs(self):
+        l = []
+        for matching in self.matchings:
+            n = matching[0]
+            m = matching[1]
+            t = matching[2]
+            l.append(abs(n.loc - m.loc) + (t - n.t) + (t - m.t))
+        return l
+
+    def skill_range(self):
+        _min = min(self, key=lambda x: x.loc)
+        _max = max(self, key=lambda x: x.loc)
+        return _max.loc - _min.loc
+
+#10.1007/978-3-319-89441-6_11
+class BienkowskiSpace(Space):
+    def __init__(self, alpha, beta):
+        super().__init__()
+        self.title = "Bienkowski Space"
+        self.alpha = alpha #must be greater than 0
+        self.beta = beta #must be greater than 1
+
+    def budget(self, n, t):
+        return self.alpha * (t - n.t)
+
+    def budget_suff(self, p, q, t):
+        return self.budget(p, t) + self.budget(q, t) >= p - q
+
+    def budget_bal(self, p, q, t):
+        return self.budget(p, t) <= self.beta * self.budget(q, t) and self.budget(q, t) <= self.beta * self.budget(p, t)
+
+    def should_match(self, p, q, t):
+        if self.budget_suff(p, q, t) and self.budget_bal(p, q, t):
+            return self.match(p, q, t)
+        return False
+
 #https://link.springer.com/article/10.1007/s00224-019-09963-7
 class AzarFananiSpace(Space):
     def __init__(self, E=1):
@@ -65,7 +104,7 @@ class AzarFananiSpace(Space):
     def should_match(self, p, q, t):
         #print(t, "<>", p.t + ((p-q)/0.01), p.t + ((p-q)/0.1), p.t + ((p-q)/1))
         if p.t >= q.t and t == p.t + ((p-q)/self.E):
-            return self.match(p, q)
+            return self.match(p, q, t)
         return False
 
 class BTrialSpace(Space):
@@ -80,9 +119,31 @@ class BTrialSpace(Space):
     def should_match(self, p, q, t):
         chance = random.random()
         if 1/abs(p.loc-q.loc) >= chance: #I think mathematically I'm supposed to use <= to represent a bernoulli trial but I'm getting better results this way.
-            return self.match(p, q)
-        else:
-            return False
+            return self.match(p, q, t)
+        return False
+
+#This greedy algorithm is presented as a preliminary in https://arxiv.org/pdf/2210.07018.pdf
+class GreedySpace(Space):
+    def __init__(self):
+        super().__init__()
+        self.title = "Greedy Algorithm"
+
+    def should_match(self, p, q, t):
+        if (t - p.t) + (t - q.t) >= abs(p.loc - q.loc):
+            return self.match(p, q, t)
+        return False
+
+class CustomSpace(Space):
+    def __init__(self):
+        super().__init__()
+        self.title = "Custom Space"
+
+    def should_match(self, p, q, t):
+        normal = 1 - abs(p.loc - q.loc)/self.skill_range()
+        #The closer normal is to 0, the closer the values are.
+        if abs(q.t - p.t) * normal > 500:
+            return self.match(p, q, t)
+        return False
 
 def create_requests(seed, length, spawn_chance, min_loc, max_loc):
     random.seed(seed)
@@ -94,6 +155,19 @@ def create_requests(seed, length, spawn_chance, min_loc, max_loc):
             l.append(None)
     l = l + [None]*99999
     return l
+
+def create_requests_dist_skill(seed, length, spawn_chance, mean_loc):
+    random.seed(seed)
+    l = []
+    dist = np.random.normal(loc=mean_loc, scale=150.0, size=length)
+    for i in range(0, length):
+        if random.random() <= spawn_chance:
+            l.append((dist[0], i))
+            dist = dist[1:]
+        else:
+            l.append(None)
+    l = l + [None]*99999
+    return l       
 
 def draw_data(spaces):
     fig = plt.figure()
@@ -124,12 +198,18 @@ def draw_stats(spaces):
 
     plt.show()
 
-reqs = create_requests(time.time(), 6000, 0.009, 100, 2400)
-afspace = AzarFananiSpace(1)
-afspace2 = AzarFananiSpace(0.1)
+#reqs = create_requests(time.time(), 3000, 0.009, 1000, 2400)
+reqs = create_requests_dist_skill(time.time(), 30000, 0.009, 1600)
+#afspace = AzarFananiSpace(1)
+#afspace2 = AzarFananiSpace(0.001)
 btspace = BTrialSpace()
-spaces = [afspace, afspace2, btspace]
+gspace = GreedySpace()
+bkspace = BienkowskiSpace(0.5, 2)
+#cust = CustomSpace()
+spaces = [gspace, bkspace, btspace]
 for t, r in enumerate(reqs):
+    if t % 1000 == 0:
+        print(f"T: {t}")
     for space in spaces:
         if r:
             space.add(r[0], r[1])
@@ -138,6 +218,7 @@ for t, r in enumerate(reqs):
                 if node == other:
                     continue
                 m = space.should_match(node, other, t)
-
+for space in spaces:
+    print(f"{space.title} Avg Cost: {np.average(space.get_all_costs())}")
 draw_stats(spaces)
 draw_data(spaces)
